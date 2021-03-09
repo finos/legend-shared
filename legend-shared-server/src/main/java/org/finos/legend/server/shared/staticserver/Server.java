27faba1dc774318ec5fd8cb7b0e2f9b30c233012
Pierre De Belen <pierre.debelen@gs.com>
2021-03-09 18:23:27 -0500

    Fix Sonar issues (#91)

--- legend-shared-server/src/main/java/org/finos/legend/server/shared/staticserver/Server.java
+++ legend-shared-server/src/main/java/org/finos/legend/server/shared/staticserver/Server.java
@@ -20,80 +20,91 @@ import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
 import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Enumeration;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.jar.Manifest;
 
 public class Server extends Application<org.finos.legend.server.shared.staticserver.StaticServerConfiguration>
 {
-  public static void main(String[] args) throws Exception
-  {
-    if (args.length > 1 && !Files.exists(new File(args[1]).toPath()))
+    public static void main(String[] args) throws Exception
     {
-      System.out.println("WARNING '" + args[1] + "' can't be found ");
-      System.out.println("Please use -v option to map the 'config' folder to an external folder containing config.json.\n"
-                         + "Example: docker run --name shared --publish 8080:8080 -v ~/work/code/legend-shared/legend-shared-server/src/main/resources/:/config -d finos/legend-shared-server:0.6.0-SNAPSHOT");
-
-      URL resource = Server.class.getClassLoader().getResource("config.json");
-      System.out.println("Using the default config sourced from :\n" + Objects.requireNonNull(resource).getPath());
-
-      String content = new Scanner(resource.openStream()).useDelimiter("\\A").next();
-      System.out.println("Content:\n" + content);
-
-      File file = File.createTempFile("tempConfig", ".json");
-      new FileOutputStream(file).write(content.getBytes());
-      System.out.println("Writing to temp file:" + file.getAbsolutePath());
-
-      args[1] = file.getAbsolutePath();
-    }
-    new org.finos.legend.server.shared.staticserver.Server().run(args);
-  }
-
-  private String getStaticPathFromManifest()
-  {
-    try
-    {
-      Enumeration<URL> resources = getClass().getClassLoader().getResources("META-INF/MANIFEST.MF");
-      while (resources.hasMoreElements())
-      {
-        Manifest manifest = new Manifest(resources.nextElement().openStream());
-        String path = manifest.getMainAttributes().getValue("Static-Root");
-        if (!Strings.isNullOrEmpty(path))
+        if (args.length > 1 && !Files.exists(new File(args[1]).toPath()))
         {
-          return path;
+            System.out.println("WARNING '" + args[1] + "' can't be found ");
+            System.out.println("Please use -v option to map the 'config' folder to an external folder containing config.json.\n"
+                    + "Example: docker run --name shared --publish 8080:8080 -v ~/work/code/legend-shared/legend-shared-server/src/main/resources/:/config -d finos/legend-shared-server:0.6.0-SNAPSHOT");
+
+            URL resource = Server.class.getClassLoader().getResource("config.json");
+            System.out.println("Using the default config sourced from :\n" + Objects.requireNonNull(resource).getPath());
+
+            try (Scanner scanner = new Scanner(resource.openStream()))
+            {
+                String content = scanner.useDelimiter("\\A").next();
+                System.out.println("Content:\n" + content);
+                FileAttribute<Set<PosixFilePermission>> attr = PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rw-------"));
+                File file = Files.createTempFile("tempConfig", ".json", attr).toFile();
+                try (FileOutputStream fos = new FileOutputStream(file))
+                {
+                    fos.write(content.getBytes());
+                }
+                System.out.println("Writing to temp file:" + file.getAbsolutePath());
+                args[1] = file.getAbsolutePath();
+            }
         }
-      }
-    } catch (IOException ignored)
-    {
-      // ignore
+        new org.finos.legend.server.shared.staticserver.Server().run(args);
     }
-    return null;
-  }
 
-  @Override
-  public void initialize(Bootstrap<org.finos.legend.server.shared.staticserver.StaticServerConfiguration> bootstrap)
-  {
-    // Enable variable substitution with environment variables
-    bootstrap.setConfigurationSourceProvider(
-        new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
-            new EnvironmentVariableSubstitutor(true)
-        )
-    );
-    String staticPath = getStaticPathFromManifest();
-    bootstrap.addBundle(new org.finos.legend.server.shared.staticserver.StaticServerBundle<>(
-        staticPath, Function.identity()));
-  }
+    private String getStaticPathFromManifest()
+    {
+        try
+        {
+            Enumeration<URL> resources = getClass().getClassLoader().getResources("META-INF/MANIFEST.MF");
+            while (resources.hasMoreElements())
+            {
+                Manifest manifest = new Manifest(resources.nextElement().openStream());
+                String path = manifest.getMainAttributes().getValue("Static-Root");
+                if (!Strings.isNullOrEmpty(path))
+                {
+                    return path;
+                }
+            }
+        }
+        catch (IOException ignored)
+        {
+            // ignore
+        }
+        return null;
+    }
 
-  @Override
-  public void run(org.finos.legend.server.shared.staticserver.StaticServerConfiguration staticServerConfiguration,
-                  Environment environment)
-  {
-  }
+    @Override
+    public void initialize(Bootstrap<org.finos.legend.server.shared.staticserver.StaticServerConfiguration> bootstrap)
+    {
+        // Enable variable substitution with environment variables
+        bootstrap.setConfigurationSourceProvider(
+                new SubstitutingSourceProvider(bootstrap.getConfigurationSourceProvider(),
+                        new EnvironmentVariableSubstitutor(true)
+                )
+        );
+        String staticPath = getStaticPathFromManifest();
+        bootstrap.addBundle(new org.finos.legend.server.shared.staticserver.StaticServerBundle<>(
+                staticPath, Function.identity()));
+    }
+
+    @Override
+    public void run(org.finos.legend.server.shared.staticserver.StaticServerConfiguration staticServerConfiguration,
+                    Environment environment)
+    {
+    }
 }
